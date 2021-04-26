73ef56342d46f0ff94599916d2c02745b842804f
tobyatgithub <toby.fangyuan@gmail.com>
2021-04-26 00:39:09 -0700

    default export and named export.

--- react-app/src/Teacher.js
+++ react-app/src/Teacher.js
@@ -1,6 +1,11 @@
 import { Person } from "./Person";
 
-export class Teacher extends Person {
+export function promote() {}
+
+// Default -> import ... from ''
+// Named -> import { ... } from ''
+
+export default class Teacher extends Person {
   constructor(name, degree) {
     super(name);
     this.degree = degree;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Teacher } from "./Teacher";
+import Teacher, { promote } from "./Teacher";
 
 const element = <h1>Hello World</h1>;
 ReactDOM.render(element, document.getElementById("root")); // element to render, and where it shall render it
