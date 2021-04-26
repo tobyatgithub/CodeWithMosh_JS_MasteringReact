a2dd9f30b315dc06df83391cb7c2560444ffd579
tobyatgithub <toby.fangyuan@gmail.com>
2021-04-26 16:00:18 -0700

    conditional rendering.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -4,7 +4,7 @@ class Counter extends Component {
   state = {
     count: 10,
     imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
 
   styles = {
@@ -12,17 +12,26 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {" "}
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt="" /> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
   }
