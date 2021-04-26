dd3bc90a67988c909ae3d61c713bdac52d83929f
tobyatgithub <toby.fangyuan@gmail.com>
2021-04-26 16:10:14 -0700

    handling events.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -24,12 +24,21 @@ class Counter extends Component {
     );
   }
 
+  handleIncrement() {
+    console.log("Increment clicked");
+  }
+
   render() {
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt="" /> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         {this.state.tags.length === 0 && "Please create a new tag!"}
         {this.renderTags()}
       </div>
