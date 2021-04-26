d3d86e82a60a2677a90ffeb745d6bcddabca09b1
tobyatgithub <toby.fangyuan@gmail.com>
2021-04-26 15:54:02 -0700

    render lists.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -4,6 +4,7 @@ class Counter extends Component {
   state = {
     count: 10,
     imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   styles = {
@@ -17,6 +18,11 @@ class Counter extends Component {
         {/* <img src={this.state.imageUrl} alt="" /> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
