83f10ca091cb29699ba361f791de4b0ba3db219d
tobyatgithub <toby.fangyuan@gmail.com>
2021-04-26 05:28:18 -0700

    rendering class dynamically + encapsulation.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 10,
     imageUrl: "https://picsum.photos/200",
   };
 
@@ -15,13 +15,18 @@ class Counter extends Component {
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
