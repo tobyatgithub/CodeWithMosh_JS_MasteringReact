053d5883aea50152bd50bcdd9efb960d6667bde4
tobyatgithub <toby.fangyuan@gmail.com>
2021-05-03 17:19:00 -0700

    the table is working for the vidly exercise.

---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  constructor() {
+    super();
+    this.handleDeleter = this.handleDeleter.bind(this);
+  }
+
+  handleDeleter = (movie) => {};
+
+  renderTableData() {
+    return this.state.movies.map((movie) => {
+      const {
+        _id,
+        title,
+        genre,
+        numberInStock,
+        dailyRentalRate,
+        publishDate,
+      } = movie;
+
+      return (
+        <tr key={_id}>
+          <td>{title}</td>
+          <td>{genre.name}</td>
+          <td>{numberInStock}</td>
+          <td>{dailyRentalRate}</td>
+        </tr>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 id="title">Movie Database</h1>
+        <table class="table" id="movies">
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+          <tbody>{this.renderTableData()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
