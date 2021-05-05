2325bca74aa28917659d73fccd8e58e93ff7828d
tobyatgithub <toby.fangyuan@gmail.com>
2021-05-04 20:35:11 -0700

    table and delete button done.

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -11,7 +11,12 @@ class Movies extends Component {
     this.handleDeleter = this.handleDeleter.bind(this);
   }
 
-  handleDeleter = (movie) => {};
+  handleDeleter = (movie) => {
+    const new_movies = this.state.movies.filter(
+      (exist_movie) => exist_movie != movie
+    );
+    this.setState({ movies: new_movies });
+  };
 
   renderTableData() {
     return this.state.movies.map((movie) => {
@@ -30,6 +35,14 @@ class Movies extends Component {
           <td>{genre.name}</td>
           <td>{numberInStock}</td>
           <td>{dailyRentalRate}</td>
+          <button
+            onClick={() => {
+              this.handleDeleter(movie);
+            }}
+            className="btn btn-secondary btn-sm"
+          >
+            Delete
+          </button>
         </tr>
       );
     });
@@ -45,6 +58,7 @@ class Movies extends Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th>Action</th>
           </tr>
           <tbody>{this.renderTableData()}</tbody>
         </table>
