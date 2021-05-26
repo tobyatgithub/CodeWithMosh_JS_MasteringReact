f9984fe7b57726101b4496eb277760ec9ef044f0
tobyatgithub <toby.fangyuan@gmail.com>
2021-05-26 15:20:30 -0700

    25 - wrapping up sorting session.

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -56,17 +56,14 @@ class Movies extends Component {
     this.setState({ sortColumn });
   };
 
-  render() {
-    const { length: numOfMovies } = this.state.movies;
+  getPageData = () => {
     const {
       pageSize,
       currentPage,
       sortColumn,
-      allGenres,
       currentGenre,
       movies: allMovies,
     } = this.state;
-    if (numOfMovies === 0) return <p>There are no movies in the database.</p>;
 
     const filtered =
       currentGenre && currentGenre._id
@@ -78,6 +75,17 @@ class Movies extends Component {
 
     const movies = paginate(sorted, currentPage, pageSize);
 
+    return { totalCount: filtered.length, data: movies };
+  };
+
+  render() {
+    const { length: numOfMovies } = this.state.movies;
+    const { pageSize, currentPage, sortColumn, allGenres, currentGenre } =
+      this.state;
+    if (numOfMovies === 0) return <p>There are no movies in the database.</p>;
+
+    const { totalCount, data: movies } = this.getPageData();
+
     return (
       <div className="row">
         <div className="col-3">
@@ -88,7 +96,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Showing {filtered.length} movies in the database.</p>
+          <p>Showing {totalCount} movies in the database.</p>
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
@@ -97,7 +105,7 @@ class Movies extends Component {
             onSort={this.handleSort}
           />
           <Pagination
-            itemsCount={filtered.length}
+            itemsCount={totalCount}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
