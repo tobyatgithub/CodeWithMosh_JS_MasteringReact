7d2a369747b525c5f79a730c52ca8ffef94b1971
tobyatgithub <toby.fangyuan@gmail.com>
2021-05-26 15:01:43 -0700

    22, extracting tableheader, table body, render cell content dynamically, and give unique keys to each td tr element.

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -52,14 +52,7 @@ class Movies extends Component {
     this.setState({ currentGenre: genre, currentPage: 1 });
   };
 
-  handleSort = (path) => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
 
@@ -98,6 +91,7 @@ class Movies extends Component {
           <p>Showing {filtered.length} movies in the database.</p>
           <MoviesTable
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDeleter}
             onSort={this.handleSort}
