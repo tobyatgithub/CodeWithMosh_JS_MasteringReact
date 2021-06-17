e85f648d4c1f1b4d914bce399f2837b0130f2ed3
tobyatgithub <toby.fangyuan@gmail.com>
2021-06-16 17:11:53 -0700

    1 - 7, buiding a bootstrap form, handle the submission, use refs or autoFocus for highlights, use controlled elements to pass data, and how to handle multiple inputs.

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,8 +1,56 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // call the server, save, redirect
+    console.log("Submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
-    return <h1>Login</h1>;
+    const { account } = this.state;
+
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              id="username"
+              name="username"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
   }
 }
 
