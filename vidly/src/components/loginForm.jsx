5920b83f39316fd0bb01a5c6a2aa3f827509b787
tobyatgithub <toby.fangyuan@gmail.com>
2021-06-20 17:39:31 -0700

    14. validating a form using Joi.

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -9,24 +9,18 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false,
-    });
-    console.log(result);
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
 
     const errors = {};
-
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
@@ -42,10 +36,10 @@ class LoginForm extends Component {
 
   validateProperty = ({ name, value }) => {
     if (name === "username") {
-      if (value.trim() === "") return "Username is required2.";
+      if (value.trim() === "") return "Username is required.";
     }
     if (name === "password") {
-      if (value.trim() === "") return "Password is required2.";
+      if (value.trim() === "") return "Password is required.";
     }
   };
 
